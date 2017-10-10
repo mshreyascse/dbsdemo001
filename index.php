199138ba780cd6f553413e6e4ab51ea418ff890d
mshreyascse <32483977+mshreyascse@users.noreply.github.com>
2017-10-10 13:33:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 echo "<h1>Hello World V 2.0 </h1>";
 echo "<p> Hello World from Heroku </p>";
 echo "<p> This is a new paragraph </p>";
+echo "<p><a href='peopleapi.php'>Link to People API</a></p>";
